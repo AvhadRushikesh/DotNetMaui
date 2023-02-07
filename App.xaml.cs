7f8c74a7dd0e06518ded3d36acac8bfac190eee2
rushi-smb <rushi@smbios.co.in>
2023-02-07 21:45:07 +0530

    Flyout Page

--- App.xaml.cs
+++ App.xaml.cs
@@ -14,6 +14,6 @@ public partial class App : Application
         navPage.BarBackground = Colors.Chocolate;
         navPage.BarTextColor = Colors.White;
 
-        MainPage = navPage;
+        MainPage = new FlyoutPageDemo();
     }
 }
