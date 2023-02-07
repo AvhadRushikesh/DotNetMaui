6237382a461386ccf6557544417807c7364428ce
rushi-smb <rushi@smbios.co.in>
2023-02-07 22:05:30 +0530

    Tabbed Page

--- App.xaml.cs
+++ App.xaml.cs
@@ -14,6 +14,6 @@ public partial class App : Application
         navPage.BarBackground = Colors.Chocolate;
         navPage.BarTextColor = Colors.White;
 
-        MainPage = new FlyoutPageDemo();
+        MainPage = new TabbedPageDemo();
     }
 }
