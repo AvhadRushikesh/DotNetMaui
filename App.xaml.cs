ae89607450c714bdadabdec3662e6d704d188fdd
rushi-smb <rushi@smbios.co.in>
2023-02-08 11:23:21 +0530

    Stack Layout

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,11 +9,11 @@ public partial class App : Application
         //MainPage = new AppShell();
         //MainPage = new FirstContentPage();
 
-        var navPage = new NavigationPage(new MainPage());
+        //var navPage = new NavigationPage(new MainPage());
+        //navPage.BarBackground = Colors.Chocolate;
+        //navPage.BarTextColor = Colors.White;
+        //MainPage = new TabbedPageDemo();
 
-        navPage.BarBackground = Colors.Chocolate;
-        navPage.BarTextColor = Colors.White;
-
-        MainPage = new TabbedPageDemo();
+        MainPage = new StackLayoutDemo();
     }
 }
